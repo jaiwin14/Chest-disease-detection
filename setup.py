b6e8999d341fcf5a0fb04f00cbeb5c017edbaa9c
jaiwin14 <jwinmehta@gmail.com>
2025-04-11 13:04:23 +0530

    changes made

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "MLOPs-Production-Ready-Deep-Learning-Project"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "Chest-disease-detection"
+AUTHOR_USER_NAME = "jaiwin14"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "jwinmehta@gmail.com"
 
 
 setuptools.setup(
