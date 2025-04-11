267e97148a03f55285584f948a9b79faba1dd940
jaiwin14 <jwinmehta@gmail.com>
2025-04-11 14:53:09 +0530

    data ingestion2 + preparebasemodel

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_dataingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_preparebasemodel import PrepareBaseModelTrainingPipeline
 
 
-logger.info("Welcome to my project")
-logger.info("Ayoo")
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
